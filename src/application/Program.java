a7a42c44dc0060de2727592d6ad6baacecb29b40
Leonardo Kons <leonardolkkons@gmail.com>
2022-07-21 21:08:47 -0300

    Configurações iníciais de classse vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,11 @@ package src.application;
 
 import src.db.DB;
 import src.model.entities.Department;
+import src.model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Program {
     public static void main(String[] args)
@@ -12,8 +15,11 @@ public class Program {
         DB.closeConnection();
 
         Department dep = new Department(1, "leo");
+        Seller sell = new Seller(1, "Leo","leozin.com", new Date(), 300.00, dep);
 
-        System.out.println(dep);
+        System.out.println("O obj Seller é: " + sell);
+
+        DB.closeConnection();
 
     }
 }
